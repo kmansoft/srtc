d699b71ddca7789f76ce381f4b81f885de873f46
Kostya Vasilyev <kman@fastmail.com>
2026-01-28 22:57:09 -0800

    Add comments to Google VLA payload generation

--- src/simulcast_layer.cpp
+++ src/simulcast_layer.cpp
@@ -7,7 +7,12 @@ void buildGoogleVLA(ByteBuffer& buf, uint8_t ridId, const std::vector<std::share
 {
     buf.clear();
 
-    if (list.empty() || list.size() > 4) {
+    const auto listSize = list.size();
+    if (listSize == 0 || listSize > 4) {
+        // Invalid input
+        return;
+    }
+    if (ridId > 3) {
         // Invalid input
         return;
     }
@@ -16,14 +21,23 @@ void buildGoogleVLA(ByteBuffer& buf, uint8_t ridId, const std::vector<std::share
 
     // https://webrtc.googlesource.com/src/+/refs/heads/main/docs/native-code/rtp-hdrext/video-layers-allocation00
 
-    w.writeU8((ridId << 6) | ((list.size() - 1) << 4) | 0x01);
+    // |RID| NS| sl_bm |
+    w.writeU8((ridId << 6) | ((listSize - 1) << 4) | 0x01);
+    // |sl0_bm |sl1_bm |
+    // |sl2_bm |sl3_bm | - none needed because sl_bm is not zero
+    // |#tl|#tl|#tl|#tl| - all zeros meaning one temporal layer per spatial layer
     w.writeU8(0);
 
+    // bitrate in kpbs per temporal layer
     for (const auto& layer : list) {
         w.writeLEB128(layer->kilobits_per_second);
     }
 
+    // resolution and framerate per spatial layer
     for (const auto& layer : list) {
+        // width-1
+        // height-1
+        // max framerate
         w.writeU16(layer->width - 1);
         w.writeU16(layer->height - 1);
         w.writeU8(layer->frames_per_second);
