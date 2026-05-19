ebbd7af001209712dd07bd06d37c9a06e31350a9
Kostya Vasilyev <kman@fastmail.com>
2026-05-18 21:20:46 -0700

    Add message builder

---
+++ include/sctp/sctp_message_builder.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "srtc/byte_buffer.h"
+
+#include <cstdint>
+
+namespace srtc::sctp {
+
+class SctpMessageBuilder
+{
+public:
+    SctpMessageBuilder(uint16_t srcPort, uint16_t dstPort, uint32_t verificationTag);
+    ~SctpMessageBuilder();
+
+    void addChunk(uint8_t type, uint8_t flags, const uint8_t* data, size_t length);
+    void addChunk(uint8_t type, uint8_t flags, const ByteBuffer& buf);
+
+    [[nodiscard]] srtc::ByteBuffer build();
+
+private:
+    srtc::ByteBuffer mBuf;
+    srtc::ByteWriter mWriter;
+};
+
+} // namespace srtc::sctp
